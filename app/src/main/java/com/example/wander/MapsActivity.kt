7d941b20142f833a9da51ae04330db00821629e3
muhammedkhaled <muhammedkhaleda@gmail.com>
2021-06-19 17:00:29 +0200

    get current location

--- app/src/main/java/com/example/wander/MapsActivity.kt
+++ app/src/main/java/com/example/wander/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.wander
 
 import android.Manifest
+import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.location.Geocoder
@@ -19,20 +20,26 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.example.wander.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.model.*
+import com.google.android.gms.tasks.Task
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var locationPermissionGranted: Boolean = false
 
     private val TAG = MapsActivity::class.java.simpleName
     private lateinit var binding: ActivityMapsBinding
     private var currentMarker: Marker? = null
     private val REQUEST_LOCATION_PERMISSION = 1
+    private val REQUEST_CHECK_SETTINGS = 0
+
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var currentLocation: Location? = null
+    private val defaultLocation = LatLng(30.033333, 31.233334)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,45 +47,42 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-/*        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
- val mapFragment = supportFragmentManager
+        val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        */
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fetchLocation()
-
+        createLocationRequest()
     }
 
 
-    private fun fetchLocation() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
             ActivityCompat.requestPermissions(
-                this,
-                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_LOCATION_PERMISSION
             )
-            return
         }
-            val task = fusedLocationClient?.lastLocation
-            task?.addOnSuccessListener { location ->
-                if (location != null) {
-                    this.currentLocation = location
-                    val mapFragment = supportFragmentManager
-                        .findFragmentById(R.id.map) as SupportMapFragment
-                    mapFragment.getMapAsync(this)
-                }
-            }
 
+/*        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location: Location? ->
+                this.currentLocation = location
+                Log.d(TAG, "Location long:  ${location?.longitude}")
+                Log.d(TAG, "Location lat:  ${location?.latitude}")
+                // Got last known location. In some rare situations this can be null.
+                drawMarker(LatLng(location?.longitude!!, location?.longitude!!))
+            }*/
+        map.isMyLocationEnabled = true
     }
-
-
 
 
     private fun enableMyLocation() {
@@ -95,35 +99,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
                 REQUEST_LOCATION_PERMISSION
             )
+
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(Manifest.permission.ACCESS_COARSE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+            locationPermissionGranted = false
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
             return
-        } else
-            map.isMyLocationEnabled = true
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                this.currentLocation = location
-                Log.d(TAG, "Location long:  ${location?.longitude}")
-                Log.d(TAG, "Location lat:  ${location?.latitude}")
-                // Got last known location. In some rare situations this can be null.
-            }
+        } else {
+            locationPermissionGranted = true
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    this.currentLocation = location
+                    Log.d(TAG, "Location long:  ${location?.longitude}")
+                    Log.d(TAG, "Location lat:  ${location?.latitude}")
+                    // Got last known location. In some rare situations this can be null.
+                    drawMarker(LatLng(location?.longitude!!, location?.longitude!!))
+                }
+        }
+        map.isMyLocationEnabled = true
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        locationPermissionGranted = false
+
         when (requestCode) {
             REQUEST_LOCATION_PERMISSION -> {
                 // If request is cancelled, the result arrays are empty.
                 if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
                     // Permission is granted. Continue the action or workflow
                     // in your app.
 //                    enableMyLocation()
-                    fetchLocation()
+                    getLocationPermission()
+                    locationPermissionGranted = true
 
                 } else {
                     // Explain to the user that the feature is unavailable because
@@ -131,7 +152,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                     // At the same time, respect the user's decision. Don't link to
                     // system settings in an effort to convince the user to change
                     // their decision.
-                    Toast.makeText(this, "you must grant the location permission to continue", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        this,
+                        "you must grant the location permission to continue",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
                 return
             }
@@ -140,6 +165,69 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             else -> {
                 // Ignore all other requests.
             }
+        }
+
+        updateLocationUI()
+
+    }
+
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                currentLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        currentLocation = task.result
+                        if (currentLocation != null) {
+                            /*map?.moveCamera(
+                                CameraUpdateFactory.newLatLngZoom(
+                                    LatLng(
+                                        currentLocation!!.latitude,
+                                        currentLocation!!.longitude
+                                    ), 10f
+                                )
+                            )*/
+                            drawMarker(LatLng(currentLocation!!.latitude, currentLocation!!.longitude))
+                        }
+
+                    } else {
+                        Log.d(TAG, "Current location is null. Using defaults.")
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        /*map?.moveCamera(
+                            CameraUpdateFactory
+                                .newLatLngZoom(defaultLocation, 10f)
+                        )*/
+                        drawMarker(LatLng(defaultLocation.latitude, defaultLocation.longitude))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
         }
     }
 
@@ -154,55 +242,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        val zoomLevel = 10f
-        // Add a marker in Sydney and move the camera
-        /*val shubra = LatLng(30.128611, 31.242222)
-        map.addMarker(MarkerOptions().position(shubra).title("Marker in Shubra"))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(shubra, zoomLevel))
-        setMapLongClick(map)
-        setPoiClick(map)
-        setMapStyle(map)
-        enableMyLocation()*/
-//        enableMyLocation()
-        fetchLocation()
 
-        val latLng = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
-        drawMarker(latLng)
-        map.setOnMarkerDragListener(object: GoogleMap.OnMarkerDragListener{
+        setMapLongClick(map)
+        setOnDragClick(map)
+        setMapStyle(map)
+//        enableMyLocation()
+
+        getLocationPermission()
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+
+
+////        val latLng = LatLng(currentLocation?.latitude!!, currentLocation?.longitude!!)
+
+
+    }
+
+    private fun drawMarker(latLng: LatLng) {
+        val snippet = String.format(
+            Locale.getDefault(),
+            "Lat: %1$.5f, Long: %2$.5f",
+            latLng.latitude,
+            latLng.longitude
+        )
+
+
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+        currentMarker = map.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title("current location")
+                .snippet(snippet)
+                .draggable(true)
+        )
+        currentMarker?.showInfoWindow()
+    }
+
+
+    private fun setOnDragClick(map: GoogleMap) {
+        map.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
             override fun onMarkerDragStart(p0: Marker?) {
             }
 
             override fun onMarkerDrag(p0: Marker?) {
-                if (currentMarker != null){
-                    currentMarker?.remove()
-
-                    val newLatLong = LatLng(p0?.position!!.latitude, p0?.position!!.longitude)
-                    drawMarker(newLatLong)
-                }
             }
 
             override fun onMarkerDragEnd(p0: Marker?) {
+                if (currentMarker != null) {
+                    currentMarker?.remove()
+                }
+                val newLatLong = LatLng(p0?.position!!.latitude, p0?.position!!.longitude)
+                drawMarker(newLatLong)
             }
 
         })
     }
-
-    private fun drawMarker(latLng: LatLng) {
-        val markerOptions = MarkerOptions().position(latLng).title("current location")
-            .snippet(getAddress(latLng.latitude, latLng.longitude)).draggable(true)
-
-        map.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-        currentMarker = map.addMarker(markerOptions)
-        currentMarker?.showInfoWindow()
-    }
-
-    private fun getAddress(latitude: Double, longitude: Double): String? {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        val address = geocoder.getFromLocation(latitude, longitude, 1)
-        return address[0].getAddressLine(0).toString()
-    }
-
 
     private fun setMapLongClick(map: GoogleMap) {
         map.setOnMapLongClickListener { latLng ->
@@ -212,16 +310,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 latLng.latitude,
                 latLng.longitude
             )
-            map.addMarker(
+            if (currentMarker != null) {
+                currentMarker!!.remove()
+            }
+
+            currentMarker = map.addMarker(
                 MarkerOptions()
                     .position(latLng)
                     .title(getString(R.string.dropped_pin))
                     .snippet(snippet)
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .draggable(true)
             )
+            map.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            currentMarker?.showInfoWindow()
         }
 
     }
+
+    private fun createLocationRequest() {
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            // All location settings are satisfied. The client can initialize
+            // location requests here.
+
+            getDeviceLocation()
+        }
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this,
+                        REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+
+    }
+
+
+
+
 
     private fun setPoiClick(map: GoogleMap) {
         map.setOnPoiClickListener { poi ->
